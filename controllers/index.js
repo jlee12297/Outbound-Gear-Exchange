957a99aa0f18ae474fc4325c7ba69482b63e6312
Josh <jlee12297@gmail.com>
2022-08-04 10:46:08 -0700

    adding in more template code to create functioning log in

--- controllers/index.js
+++ controllers/index.js
@@ -1,40 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const apiRoutes = require("./api")
+const frontEndRoutes = require("./frontEndRoutes")
 
-router.get('/',(req,res)=>{
-    res.render("home")
-})
+router.use('/api', apiRoutes);
+router.use(frontEndRoutes)
 
-router.get("/login",(req,res)=>{
-    res.render("login")
-})
-
-router.get("/secretclub",(req,res)=>{
-    if(!req.session.user){
-        res.redirect("/login")
-    }
-    res.render("club",req.session.user)
-})
-
-router.get("/readsession",(req,res)=>{
-    res.json(req.session)
-})
-
-router.get("/addcounter",(req,res)=>{
-    if(req.session.counter){
-        req.session.counter++
-    } else {
-        req.session.counter=1
-    }
-    res.send("req.session updated")
-})
-
-router.get("/logout",(req,res)=>{
-    req.session.destroy();
-    res.json({msg:"logged out!"})
-})
-
-router.use("/api",apiRoutes)
-
-module.exports = router
+module.exports = router;

3338aa8a82299e232091e16d47c381045e8590d2
kristensantee <mikeandkris27@gmail.com>
2022-08-10 00:01:38 -0700

    email pulling from form body

--- public/js/email.js
+++ public/js/email.js
@@ -14,50 +14,50 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 //Body Parser middleware
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 document.querySelectorAll("#email-btn").addEventListenener("submit", e=>{
     e.preventDefault();
 })
 
-const output = `
-    <p>You have a new gear request from Kristen.</p>
-    <br>
-    <p>Kristen would like to request gear item 123 from your cache starting on 8/5.</p>
-    <br>
-    <p>Their message is included below:
-    This is a test!
-    </p>
-    <br>
-    <p>Sincerely,</p>
-    <p>Your Outbound Gear Exchange Team</p>
-    `;
+// const output = `
+//     <p>You have a new gear request from Kristen.</p>
+//     <br>
+//     <p>Kristen would like to request gear item 123 from your cache starting on 8/5.</p>
+//     <br>
+//     <p>Their message is included below:
+//     This is a test!
+//     </p>
+//     <br>
+//     <p>Sincerely,</p>
+//     <p>Your Outbound Gear Exchange Team</p>
+//     `;
 
 
-let transport = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'outboundgearexchange@hotmail.com',
-        pass: 'Gear4Lyfe!'
-    }
-});
+// let transport = nodemailer.createTransport({
+//     host: "smtp-mail.outlook.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: 'outboundgearexchange@hotmail.com',
+//         pass: 'Gear4Lyfe!'
+//     }
+// });
 
-const mailOptions = {
-    from: 'outboundgearexchange@hotmail.com',
-    to: 'kristen.l.santee@gmail.com',
-    subject: 'Gear Request',
-    text: 'Hello?',
-    html: output
-}
+// const mailOptions = {
+//     from: 'outboundgearexchange@hotmail.com',
+//     to: 'kristen.l.santee@gmail.com',
+//     subject: 'Gear Request',
+//     text: 'Hello?',
+//     html: output
+// }
 
-transport.sendMail(mailOptions, function(err, info) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("Email sent!", info);
-    }
-});
+// transport.sendMail(mailOptions, function(err, info) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("Email sent!", info);
+//     }
+// });
 
